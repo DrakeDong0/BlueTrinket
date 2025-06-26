98ed31e2046e8032aae0de7d8b5f282cb887ee20
dw2dong <dw2dong@uwaterloo.ca>
2025-06-25 22:52:20 -0400

    Add launch screen and begin work on login screen

---
+++ BluetrinketApp/Bluetrinket/Login.swift
@@ -0,0 +1,22 @@
+//
+//  Login.swift
+//  Bluetrinket
+//
+//  Created by Drake Dong on 2025-06-25.
+//
+
+import SwiftUI
+
+struct LoginScreen: View{
+    @State var username: String = ""
+    @State var password: String = ""
+    var body: some View {
+        ZStack{
+            Color(.c)
+        }
+    }
+}
+
+#Preview {
+    LoginScreen()
+}
