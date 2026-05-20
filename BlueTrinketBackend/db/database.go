436292a599fb081c6408ba052c5f224638d15520
dw2dong <dw2dong@uwaterloo.ca>
2026-05-19 23:50:48 -0400

    revived project

---
+++ BlueTrinketBackend/db/database.go
@@ -0,0 +1,88 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	// "time"
+
+	"github.com/DrakeDong0/BlueTrinket/BlueTrinketBackend/structs"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
+	// "github.com/DrakeDong0/BlueTrinket/BlueTrinketBackend/structs"
+)
+
+var MongoClient *mongo.Client
+
+func DBConnect() *mongo.Client {
+	// Build and connect URI
+	uri := os.Getenv("MONGO_URI")
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		fmt.Println("Error connecting to MongoDB:", err)
+		panic(err)
+	}
+	// Test connection
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	return client
+}
+
+func GetNextCustomerID(ctx context.Context, db *mongo.Client)(int,error) {
+	filter := bson.M{"_id": "customerID"}
+	update := bson.M{"$inc": bson.M{"seq": 1}}
+
+	opts := options.FindOneAndUpdate().SetReturnDocument(options.After)
+	var result struct {
+		Seq int `bson:"seq"`
+	}
+	customerIDCol := db.Database("BlueTrinket").Collection("Counters")
+	err := customerIDCol.FindOneAndUpdate(ctx, filter, update, opts).Decode(&result)
+	if err != nil {
+		return 0, fmt.Errorf("failed to get next customer ID: %w", err)
+	}
+
+	return result.Seq, nil
+}
+
+func GetUserBySub(ctx context.Context, client *mongo.Client, sub string) (bson.ObjectID, error) {
+	userCol := client.Database("BlueTrinket").Collection("Users")
+	filter := bson.M{"sub": sub}
+	var result structs.UserLookupDBObj
+
+	err := userCol.FindOne(ctx, filter).Decode(&result)
+	if err != nil {
+		return bson.NilObjectID, err
+	}
+	return result.UserID, nil
+}
+
+func GetUserByEmail(ctx context.Context, client *mongo.Client, email string) (bson.ObjectID, error) {
+	userCol := client.Database("BlueTrinket").Collection("Users")
+	filter := bson.M{"email": email}
+	var result structs.UserLookupDBObj
+
+	err := userCol.FindOne(ctx, filter).Decode(&result)
+	if err != nil {
+		return bson.NilObjectID, err
+	}
+	return result.UserID, nil
+}
+
+func DoesUserExist(ctx context.Context, client *mongo.Client, email string, sub string) (bool, error) {
+	if _, err := GetUserByEmail(ctx, client, email); err == nil {
+		return true, nil
+	}
+	if _, err := GetUserBySub(ctx, client, sub); err == nil {
+		return true, nil
+	}
+	return false, nil
+}
---
+++ BlueTrinketBackend/endpoints/endpoints.go
@@ -0,0 +1,114 @@
+package endpoints
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/DrakeDong0/BlueTrinket/BlueTrinketBackend/db"
+	"github.com/DrakeDong0/BlueTrinket/BlueTrinketBackend/structs"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var MongoClient *mongo.Client
+
+func TestEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("endpoint hit")
+	w.Write([]byte("test recieved"))
+}
+
+func AuthLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("auth login hit")
+	ctx := r.Context()
+
+	authHeader := r.Header.Get("Authorization")
+	parts := strings.SplitN(authHeader, " ", 2)
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		http.Error(w, "Invalid Authorization header", http.StatusUnauthorized)
+		return
+	}
+	accessToken := parts[1]
+
+	// Call Auth0 /userinfo endpoint
+	userinfoURL := os.Getenv("AUTH0_DOMAIN") + "userinfo"
+	req, err := http.NewRequest("GET", userinfoURL, nil)
+	if err != nil {
+		http.Error(w, "Failed to create request", http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil || res.StatusCode != http.StatusOK {
+		fmt.Println("Failed to get user info", err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	var userInfo structs.UserDBObj
+
+	if err := json.NewDecoder(res.Body).Decode(&userInfo); err != nil {
+		http.Error(w, "Failed to parse user info", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("user info: ", userInfo)
+
+	// Check for new user
+	if doesUserExist, _ := db.DoesUserExist(ctx, MongoClient, userInfo.Email, userInfo.Sub); doesUserExist {
+		fmt.Println("user already exists")
+		w.Write([]byte(`{"isNewUser": false}`))
+	} else {
+		fmt.Println("new user created")
+		err = createUser(ctx, MongoClient, userInfo)
+		if err != nil {
+			http.Error(w, "Failed to create user", http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte(`{"isNewUser": true}`))
+	}
+}
+
+func createUser(ctx context.Context, client *mongo.Client, newUser structs.UserDBObj) error {
+	// Get ID for new user
+	newUserID, err := db.GetNextCustomerID(ctx, client)
+	if err != nil {
+		return fmt.Errorf("Error:%w", err)
+	}
+	fmt.Println("new user id: ", newUserID)
+	// Create new database for user
+	userInfoCol := client.Database(strconv.Itoa(newUserID)).Collection("userInfo")
+	res, err := userInfoCol.InsertOne(ctx, newUser)
+	if err != nil {
+		return fmt.Errorf("error creating new user database:%w", err)
+	}
+	// Add user lookup to BlueTrinket user list for lookup
+	id, ok := res.InsertedID.(bson.ObjectID)
+	if !ok {
+		return fmt.Errorf("inserted ID is not an objectID")
+	}
+	lookupData := []structs.UserLookupDBObj{
+		{
+			UserID: id,
+			Email:  newUser.Email,
+		},
+		{
+			UserID: id,
+			Sub:    newUser.Sub,
+		},
+	}
+	userLookupCol := client.Database("BlueTrinket").Collection("Users")
+	_, err = userLookupCol.InsertMany(ctx, lookupData)
+	if err != nil {
+		return fmt.Errorf("error creating user lookup in BlueTrinket DB:%w", err)
+	}
+	fmt.Println("created new user successfully")
+	return nil
+}
